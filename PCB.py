751b25a55edb179ecaa43ba0e7139543d20b8cf0
Nate Devine <ndevine@conncoll.edu>
2018-10-02 22:37:18 -0400

    added mad functions

--- PCB.py
+++ PCB.py
@@ -45,6 +45,33 @@ class PCB(object):
             
         print("Of the processes you inputted, the active processes are: ", active_list)
         print("Of the processes you inputted, the inactive processes are: ", inactive_list)
+        
+    def add_new_processe(self):
+        current_processes = self.processes
+            
+        loopFlag = True
+        while loopFlag:
+            try:
+                process = input("Please enter a process name and a process definition seperated by a comma. When you are finished, please type \"done\": ")
+                
+                process = ''.join(process.split())
+                
+                if process == "done":
+                    loopFlag = False
+                    return current_processes
+                    
+                elif process.split(",")[0].lower() in self.processes:
+                    print("You have entered a duplicate process definition")
+                    
+                elif process.split(",")[1].lower() != 'true' or process.split(",")[1].lower() != 'false':
+                    print("The process definition you entered was invalid")
+                
+                else:
+                    current_processes[process.split(",")[0]] = process.split(",")[1]
+            
+            except ValueError:
+                print("Error. Please try again")
+        
 def main():
     processes = {}
 
@@ -56,7 +83,7 @@ def main():
             processes[letter] = False
 
     print(processes)
-            
+    
     PCB_obj = PCB(processes)
     
     PCB_obj.print_active_processes(PCB_obj.getInput())
@@ -64,11 +91,3 @@ def main():
             
 if __name__ == '__main__':
     main()
-
-        
-        
-
-
-
-        
-main()
