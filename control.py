2502319a9e558b1ec71291ac7d17c16d9e6faa91
Elijah Pineda <epinedaleyva@student.unimelb.edu.au>
2018-10-29 12:27:57 -0400

    added accesbility to scheduling function

--- control.py
+++ control.py
@@ -1,11 +1,12 @@
 from PCB import *
 from PCB_utils import *
+#from Scheduling import *
 import sys
 
 #Gets the desired menu from the user and checks if it is a valid choice
 def menu():
-    print("\nYou can:\n\tType \"add\" to add a new process\n\tType \"update\" to update a process\n\tType \"print\" to see active processes\n\tType \"finish\" exit the program.")
-    process = str_verify("\nI want to: ", "update,add,print,finish", lower = "yeet, juju on the beat")
+    print("\nYou can type:\n\t\"add\" to add a new process\n\t\"update\" to update a process\n\t\"print\" to see active processes\n\t\"schedule\" to schedule the processes\n\t\"finish\" to exit the program.")
+    process = str_verify("\nI want to: ", "update,add,print,schedule,finish", lower = "yeet, juju on the beat")
 
     return process
 
@@ -24,6 +25,9 @@ def main():
 
         elif process == 'print':
             PCB_obj.print_active_processes()
+
+        elif process == 'schedule':
+            print("\nuh huh okay alright")
 
         elif process == 'finish':
             print("Smell ya later")
