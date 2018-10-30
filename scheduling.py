05b2bcecc069c603eaf80830f53871748bea8cb6
Khanh Nghiem <knghiem@conncoll.edu>
2018-10-29 23:31:24 -0400

    first come first serve done

--- scheduling.py
+++ scheduling.py
@@ -6,15 +6,70 @@ Created: 29-Oct-2018
 Last updated: 29-Oct-2018
 '''
 
-from PCB_utils import *
+from PCB import *
 import queue
 
 # First come first serve
 # @param: a list of processes
-def fcfs(process_queue):
-    while not process_queue.empty():
-        
+def fcfs(process_queue, io_duration, context_switch_penalty):
+    if process_queue.empty():
+        print("No process to schedule")
+        return
 
+    print("TIME","STATUS/PROCESS", sep="\t")
+    current_time = 0
+
+    while not process_queue.empty():
+        # serve the process at front of the queue
+        front = process_queue.get()
+
+        # get relevant
+        key = front.getKey()
+        time_created = front.getTimeCreated()
+        service_time = front.getServiceTime()
+        io_freq = front.getIOFreq()
+
+        # the system is idle until the process arrives
+        if current_time < time_created:
+            print(current_time, "idle", sep="\t")
+
+        while (current_time < time_created):
+            current_time += 1
+
+        # serve the front process
+        print(current_time, "process "+str(key), sep="\t")
+
+        # keep track of time served for the process
+        time_served = 0
+
+        # loop until process completes
+        while (service_time > 0):
+            service_time -= 1
+            time_served += 1
+            current_time += 1
+
+            # check for io event
+            if (io_freq > 0 and service_time > 0 and time_served % io_freq == 0):
+                if (context_switch_penalty > 0):
+                    print(current_time, "switch", sep="\t")
+                    current_time += context_switch_penalty
+
+                # announce io event
+                print(current_time, "io", sep="\t")
+                current_time += io_duration
+
+                # switch back to the process
+                if (context_switch_penalty > 0):
+                    print(current_time, "switch", sep="\t")
+                    current_time += context_switch_penalty
+                print(current_time, "process "+str(key), sep="\t")
+
+        # switch to either idle or the next process
+        if (context_switch_penalty > 0):
+            print(current_time, "switch", sep="\t")
+            current_time += context_switch_penalty
+
+    print(current_time, "finished", sep="\t")    
 
 
 def test():
@@ -25,7 +80,7 @@ def test():
     p5 = Process(key=5, active='True', priority=0, time_created=8, mode=0, service_time=2, io_freq=1)
     p6 = Process(key=6, active='True', priority=0, time_created=6, mode=0, service_time=5, io_freq=0)
 
-    process_queue = Queue()
+    process_queue = queue.Queue()
     process_queue.put(p1)
     process_queue.put(p2)
     process_queue.put(p3)
@@ -33,4 +88,5 @@ def test():
     process_queue.put(p5)
     process_queue.put(p6)
 
-    fcfs(process_queue)
+    fcfs(process_queue, io_duration=5, context_switch_penalty=0)
+test()
